9b65a626b640710e39c668bae3230a6672829397
Mikulas Florek <mikulas.florek@gamedev.sk>
2023-04-28 17:30:50 +0200

    synced with latest engine

--- src/myplugin.cpp
+++ src/myplugin.cpp
@@ -9,39 +9,35 @@
 using namespace Lumix;
 
 
-// each world has its own instance of this scene
-struct PluginScene : IScene {
-	PluginScene(Engine& engine, IPlugin& plugin, World& world, IAllocator& allocator)
+// each world has its own instance of this module
+struct MyModule : IModule {
+	MyModule(Engine& engine, ISystem& system, World& world, IAllocator& allocator)
 		: m_engine(engine)
-		, m_plugin(plugin)
+		, m_system(system)
 		, m_world(world)
 		, m_allocator(allocator)
 	{}
 
 	void serialize(struct OutputMemoryStream& serializer) override {
-		// save our scene data
+		// save our module data
 		serializer.write(m_some_value);
 	}
 
 	void deserialize(struct InputMemoryStream& serializer, const struct EntityMap& entity_map, i32 version) override {
-		// load our scene data
+		// load our module data
 		serializer.read(m_some_value);
 
 	}
-	IPlugin& getPlugin() const override { return m_plugin; }
+	ISystem& getSystem() const override { return m_system; }
 	World& getWorld() override { return m_world; }
 	
 	void update(float time_delta) {
 		// called each frame
 		m_some_value += time_delta; 
 	}
-	
-	void clear() override {
-		// called before scene is destroyed, clean up your data
-	}
 
 	Engine& m_engine;
-	IPlugin& m_plugin;
+	ISystem& m_system;
 	World& m_world;
 	IAllocator& m_allocator;
 	float m_some_value = 0;
@@ -49,21 +45,23 @@ struct PluginScene : IScene {
 
 
 // there will be only one instance of system
-struct PluginSystem : IPlugin {
-	PluginSystem(Engine& engine) : m_engine(engine) {}
+struct MySystem : ISystem {
+	MySystem(Engine& engine) : m_engine(engine) {}
 
-	u32 getVersion() const override { return 0; }
 	const char* getName() const override { return "myplugin"; }
 	
 	void serialize(OutputMemoryStream& serializer) const override {}
-	bool deserialize(u32 version, InputMemoryStream& serializer) override { return true; }
+	bool deserialize(i32 version, InputMemoryStream& serializer) override {
+		// do not try to deserialize newer versions, since we have no idea what's there
+		return version == 0;
+	}
 
-	void createScenes(World& world) override {
+	void createModules(World& world) override {
 		// this is when a world is created
-		// usually we want to add our scene to world here
+		// usually we want to add our module to world here
 		IAllocator& allocator = m_engine.getAllocator();
-		UniquePtr<PluginScene> scene = UniquePtr<PluginScene>::create(allocator, m_engine, *this, world, allocator);
-		world.addScene(scene.move());
+		UniquePtr<MyModule> module = UniquePtr<MyModule>::create(allocator, m_engine, *this, world, allocator);
+		world.addModule(module.move());
 	}
 
 	Engine& m_engine;
@@ -72,7 +70,7 @@ struct PluginSystem : IPlugin {
 
 LUMIX_PLUGIN_ENTRY(myplugin)
 {
-	return LUMIX_NEW(engine.getAllocator(), PluginSystem)(engine);
+	return LUMIX_NEW(engine.getAllocator(), MySystem)(engine);
 }
 
 
